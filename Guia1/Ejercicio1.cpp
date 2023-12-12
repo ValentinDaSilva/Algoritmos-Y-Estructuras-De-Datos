16b17e0d75b849d02afd5b0ce3409cd6f1082c27
ValentinDaSilva <valedasilvacatela.vdc@gmail.com>
2023-12-11 23:58:35 -0300

    comit inicial

--- Guia1/Ejercicio1.cpp
+++ Guia1/Ejercicio1.cpp
@@ -1,9 +1,7 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main (void) {
-    int nroLegajo;
-    cout <<"Me dices tu número de legajo?"<< endl;
-    cin >> nroLegajo;
-    cout <<"Ingreso de "<< nroLegajo <<" al Zinjai!"<< endl;
+
+int main(){
+    
     return 0;
 }
