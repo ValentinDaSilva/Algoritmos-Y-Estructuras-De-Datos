7a56bf1c022c3c87c2377de3aacc9fb2db016462
ValentinDaSilva <valedasilvacatela.vdc@gmail.com>
2023-12-12 02:33:24 -0300

    asd

--- Guia3/Ejercicio1.cpp
+++ Guia3/Ejercicio1.cpp
@@ -2,6 +2,22 @@
 using namespace std;
 
 int main(){
-    
+    int a , b;
+    float porcentaje;
+    cout<<"Ingrese la cantidad de preguntas: ";cin>>a;
+    cout<<"Ingrese la cantidad de respuestas correctas: ";cin>>b;
+    porcentaje = b * 100 / (float) a;
+    if(porcentaje >= 90){
+        cout<<"Nivel máximo"<<endl;
+    }
+    else if(porcentaje >= 75){
+        cout<<"Nivel medio"<<endl;
+    }
+    else if(porcentaje >= 50){
+        cout<<"Nivel regular"<<endl;
+    }
+    else{
+        cout<<"Fuera de nivel"<<endl;
+    }
     return 0;
 }
