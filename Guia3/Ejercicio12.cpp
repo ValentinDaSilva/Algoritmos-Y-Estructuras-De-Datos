6b4159057d511f88a78a593dfbd814c88ea66043
ValentinDaSilva <valedasilvacatela.vdc@gmail.com>
2023-12-13 17:03:42 -0300

    Ejercicio12

---
+++ Guia3/Ejercicio12.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int numero;
+    cout<<"Ingrese un numero: "<<endl;
+    cin>>numero;
+    switch (numero)
+    {
+        case 1: cout<<"1"<<endl;
+        case 2: cout<<"2"<<endl;
+        case 3: cout<<"3"<<endl;
+        case 4: cout<<"4"<<endl;
+        case 5: cout<<"5"<<endl;
+        case 6: cout<<"6"<<endl;
+        case 7: cout<<"7"<<endl;
+        case 8: cout<<"8"<<endl; 
+        case 9: cout<<"9"<<endl;
+    }
+    return 0;
+}
