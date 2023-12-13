37af68a31145bc048772674b3f00cc9ab24ce873
ValentinDaSilva <valedasilvacatela.vdc@gmail.com>
2023-12-13 05:47:19 -0300

    asd

---
+++ Guia3/Ejercicio7.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char modelo,motor;
+    cout<<"Ingrese el modelo: ";cin>>modelo;
+    cout<<"Ingrese el motor: ";cin>>motor;
+    switch (modelo)
+    {
+    case 'P':(motor == 16)? cout<<"Tapizado de Cuero y Llantas de aleación"<<endl:cout<<"Tapizado de Cuero, Llantas de aleación y Turbocompresor."<<endl;
+    case 'M':(motor == 16)? cout<<"Levanta vidrios eléctrico y Espejos Retráctiles."<<endl:cout<<"Levanta vidrios eléctrico, Espejos Retráctiles y Luz de Cortesía"<<endl;
+    case 'B':cout<<" Tipo de motor 16 y tipo de motor 20: Dirección asistida, ABS y Airbags"<<endl;
+    default:
+        break;
+    }
+    cout<<""<<endl;
+    return 0;
+}
