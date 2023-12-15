9864bfa698d746dadef786550ffed07db1a0cbb0
ValentinDaSilva <valedasilvacatela.vdc@gmail.com>
2023-12-15 00:13:17 -0300

    Ejercicio

--- Guia4/Pasajes por Referencia/Ejercicio6.cpp
+++ Guia4/Pasajes por Referencia/Ejercicio6.cpp
@@ -1,7 +1,19 @@
 #include<iostream>
 using namespace std;
 
+bool division(float,float,float&);
+
 int main(){
-    cout<<6 / 2<<endl;
+    float dividendo, divisor, resultado;
+    cout<<"Ingrese dividendo: ";cin>>dividendo;
+    cout<<"Ingrese dividor: ";cin>>divisor;
+    cout<<boolalpha<<division(dividendo,divisor,resultado)<<endl;
+    if(division(dividendo,divisor,resultado)) cout<<resultado<<endl;
     return 0;
+}
+
+bool division(float dividendo,float divisor,float& resultado){
+    if(divisor == 0) return false;
+    resultado = dividendo / divisor;
+    return true;
 }
