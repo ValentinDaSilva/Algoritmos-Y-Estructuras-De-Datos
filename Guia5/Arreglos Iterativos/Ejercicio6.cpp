30859410e7bf5a538060fbf3b12fd5fe94bb5668
ValentinDaSilva <valedasilvacatela.vdc@gmail.com>
2023-12-17 02:44:39 -0300

    Ejercicio 6 que habia faltado

--- Guia5/Arreglos Iterativos/Ejercicio6.cpp
+++ Guia5/Arreglos Iterativos/Ejercicio6.cpp
@@ -1,1 +1,34 @@
-//no tiene nada que ver con vectores y ya esta hecho en la guia de bucles
+#include<iostream>
+using namespace std;
+
+int existeLaLetra(char[],int,char);
+
+int main(){
+    int TL = 0, cantidades[27]= {0};
+    char letra, letras[27];
+    while(cin>>letra){
+        if(existeLaLetra(letras,TL,letra) == -1){
+            letras[TL] = letra;
+            cantidades[TL]++;
+            TL++;
+        }else{
+            cantidades[existeLaLetra(letras,TL,letra)]++;
+        }
+    }
+    for(int i = 0; i < TL; i++){
+        if(cantidades[i] == 1) cout<<"Letra "<<letras[i]<<": "<<cantidades[i]<<" vez"<<endl;
+        else cout<<"Letra "<<letras[i]<<": "<<cantidades[i]<<" veces"<<endl;
+    }
+    return 0;
+}
+
+int existeLaLetra(char v[],int TL, char letra){
+    int i = 0;
+    bool encontrada = false;
+    while(i < TL and !encontrada){
+        if(v[i] == letra) encontrada = true;
+        i++;
+    }
+    if(encontrada) return i-1;
+    else return -1;
+}
