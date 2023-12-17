90259c0233c9515e6c9b56fbff3b52f6e97f874f
ValentinDaSilva <valedasilvacatela.vdc@gmail.com>
2023-12-17 18:14:09 -0300

    Ejercicio12

--- Guia5/Arreglos Recursivos/Ejercicio12.cpp
+++ Guia5/Arreglos Recursivos/Ejercicio12.cpp
@@ -5,28 +5,28 @@ void inicializar_teclado(int[],int);
 void mostrarVector(int[],int);
 void random(int[],int);
 bool quini6(int[],int[],int);
-
+bool estaEnElVector(int[],int,int);
 int main(){
-    int TL = 6,numerosJugador[6],numerosSorteados[6], numeroJugador;
-    random(numerosSorteados,TL);
-    mostrarVector(v,TL);
+    int TL = 6,nJ[6],nS[6], numeroJugador;
+    random(nS,TL);
+    cout<<"[ ";mostrarVector(nS,TL);
     cout<<"Ingrese su numero de jugador: ";cin>>numeroJugador;
-    cout<<"Ingrese los 6 numeros que quiere jugar"<<endl; inicializar_teclado(numerosJugador,TL);
-    if(quini6(numerosJugador,numerosJugador,TL)) cout<<numerosJugador<<endl;
+    cout<<"Ingrese los 6 numeros que quiere jugar"<<endl; inicializar_teclado(nJ,TL);
+    if(quini6(nJ,nS,TL)) cout<<numeroJugador<<endl;
     else cout<<"Tus numeros no salieron elejidos, perdiste"<<endl;
     return 0;
 }
 
 bool quini6(int nJ[],int nS[], int TL){
     if(TL == 0) return false;
-    if(estaEnElVector(nS,TL,nJ[0])) return true;
-    quini6(&nJ[1],nS,--TL);
+    if(estaEnElVector(nS,6,nJ[0])) return true;
+    return quini6(&nJ[1],nS,TL-1);
 }
 
 bool estaEnElVector(int v[],int TL,int elemento){
     if(TL == 0) return false;
     if(v[0] == elemento) return true;
-    estaEnElVector(&v[1],--TL,elemento);
+    return estaEnElVector(&v[1],--TL,elemento);
 }
 
 void random(int v[],int TL){
