aaeac372f88fe2d172bb5dee7bfcced9d0e46797
ValentinDaSilva <valedasilvacatela.vdc@gmail.com>
2023-12-18 01:02:32 -0300

    Mensaje predeterminado

--- Guia5/Arreglos Recursivos/Ejercicio14.cpp
+++ Guia5/Arreglos Recursivos/Ejercicio14.cpp
@@ -5,18 +5,16 @@ void random(int[],int);
 void mostrarVector(int[],int);
 void eliminarPosicion(int[],int,int);
 int estaEnElVector(int[],int,int);
-void eliminarRepetidos(int[],int,int&);
+int eliminarRepetidos(int[],int);
 int sumaLista(int[],int);
 
 int main(){
-    int TL = 10, v[10], cantEliminados = 0,posicion;
+    int TL = 10, v[10] = {44, 4, 29, 3, 36, 6, 21, 4, 4, 41};
     random(v,TL);
     cout<<"[ ";mostrarVector(v,TL);
-    eliminarRepetidos(v,TL,cantEliminados);
-    TL -= cantEliminados;
+    TL -= eliminarRepetidos(v,TL);
     cout<<"[ ";mostrarVector(v,TL);
     cout<<"Promedio Lista: "<<sumaLista(v,TL)/(float)TL<<endl;
-    if(cin>>posicion) main();
     return 0;
 }
 
@@ -25,15 +23,14 @@ int sumaLista(int v[],int TL){
     return v[0] + sumaLista(&v[1],--TL);
 }
 
-void eliminarRepetidos(int v[],int TL,int& cantEliminados){
-    if(TL == 0) return;
+int eliminarRepetidos(int v[],int TL){
+    if(TL == 0) return 0;
     int x = estaEnElVector (&v[1],TL-1,v[0]);
     if(x < TL - 1) {
         eliminarPosicion(&v[1],TL-1,x);
-        cantEliminados++;
-        eliminarRepetidos(&v[1],TL-2,cantEliminados);
+        return 1 + eliminarRepetidos(&v[0],TL-1);
     }else{
-        eliminarRepetidos(&v[1],--TL,cantEliminados);
+        return 0 + eliminarRepetidos(&v[1],--TL);
     }
     
 }
