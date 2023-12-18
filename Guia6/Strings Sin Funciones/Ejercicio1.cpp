7c7c7ee017c177da038c3b42ada3542af2698ef9
ValentinDaSilva <valedasilvacatela.vdc@gmail.com>
2023-12-18 03:25:56 -0300

    Mensaje predeterminado

---
+++ Guia6/Strings Sin Funciones/Ejercicio1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string nombreCompleto;
+    cout<<"Ingrese su apellido, seguido de una coma y luego su nombre\nEj: Da Silva Catela, Valentin: ";
+    getline(cin,nombreCompleto);
+    
+    return 0;
+}
