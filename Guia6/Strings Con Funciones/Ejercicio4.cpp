e4a6198406463156ac999bea2f636901e2f53b87
ValentinDaSilva <valedasilvacatela.vdc@gmail.com>
2024-01-01 15:50:20 -0300

    Mensaje predeterminado

--- Guia6/Strings Con Funciones/Ejercicio4.cpp
+++ Guia6/Strings Con Funciones/Ejercicio4.cpp
@@ -5,7 +5,10 @@ int main(){
     string palabra1, palabra2;
     getline(cin,palabra1);
     getline(cin,palabra2);
-    if(palabra1.length() < palabra2.length()) cout<<palabra1<<palabra2<<palabra1<endl;
-    else cout<<palabra2<<palabra1<<palabra2<endl;
+    if(palabra1.length() < palabra2.length()){
+        cout<<palabra1<<palabra2<<palabra1<<endl;
+    }else{
+        cout<<palabra2<<palabra1<<palabra2<<endl;
+    }
     return 0;
 }
