403b9d2a0c8a23d5dcefe3df7c50941cad9cf011
ValentinDaSilva <valedasilvacatela.vdc@gmail.com>
2024-01-08 17:30:33 -0300

    auto-commit

--- Guia10/PilasDinamicas.cpp
+++ Guia10/PilasDinamicas.cpp
@@ -12,11 +12,42 @@ void inicializarPila(Pila&);
 bool estaVacia(Pila);
 void apilar(Pila&,int);
 void desapilar(Pila&);
-int elementoCima(Pila);
+int tope(Pila);
 int tamanoPila(Pila);
 
 
 int main(){
-    
+    Pila miPila;
+    inicializarPila(miPila);
     return 0;
+}
+
+void inicializarPila(Pila& miPila){
+    miPila = nullptr;
+}
+
+bool estaVacia(Pila miPila){
+    return miPila == nullptr;
+}
+
+void apilar(Pila& miPila, int elemento){
+    Pila nuevo_elemento = new Nodo;
+    nuevo_elemento->dato = elemento;
+    nuevo_elemento->siguiente = miPila;
+    miPila = nuevo_elemento;
+}
+
+void desapilar(Pila& miPila){
+    Pila aux = miPila;
+    miPila = miPila->siguiente;
+    delete aux;
+}
+
+int tope(Pila miPila){
+    return miPila->dato;
+}
+
+int tamanoPila(Pila miPila){
+    if(estaVacia(miPila)) return 0;
+    return 1 + tamanoPila(miPila->siguiente);
 }
