bff9fda0fc7f0a033b12a3b06f22a298d4857420
ValentinDaSilva <valedasilvacatela.vdc@gmail.com>
2024-01-08 18:17:45 -0300

    Mensaje predeterminado

---
+++ Guia10/ArreglosDinamicos.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+typedef int* PInt;
+
+void mostrarVector(int[],int);
+void random(int[],int);
+
+int main(){
+    int Tam;
+    cout<<"Ingrese la cantidad de elementos que quiere ingresar: ";cin>>Tam;
+    PInt v = new int[Tam];
+    random(v,Tam);
+    mostrarVector(v,Tam);
+    return 0;
+}
+void random(int v[], int TL){
+    srand(time(NULL));
+    for(int i = 0; i < TL; i++){
+        v[i] = rand()%50;
+    }
+}
+
+void mostrarVector(int v[],int TL){
+    cout<<"[ ";
+    for(int i = 0; i < TL; i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<"]"<<endl;
+}
