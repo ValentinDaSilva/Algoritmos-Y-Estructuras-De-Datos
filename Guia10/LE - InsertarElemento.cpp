4a5cbbc83f8339cd45bc5e9cd0f9f449a960d25a
ValentinDaSilva <valedasilvacatela.vdc@gmail.com>
2024-01-08 17:03:55 -0300

    auto-commit

--- Guia10/LE - InsertarElemento.cpp
+++ Guia10/LE - InsertarElemento.cpp
@@ -8,7 +8,48 @@ struct Nodo {
 typedef Nodo* PNodo;
 PNodo Lista = NULL;
 
-void insertarElemento(PNodo& Lista, string elemento){
+void mostrarLista(PNodo);
+bool buscarElemento(PNodo, string);
+void insertarAlFinal(PNodo&,string);
+void insertarOrdenado(PNodo&,string);
+void insertarAlPrincipio(PNodo&,string);
+void insertarPosicion(PNodo&,string,int);
+void eliminarTodosLosElementos(PNodo&);
+void eliminarElemento(PNodo&,string);
+void eliminarUltimaPosicion(PNodo&);
+void eliminarPrimerPosicion(PNodo&);
+void eliminarPosicion(PNodo&,int);
+void eliminarRepetidos(PNodo);
+PNodo retornarElemento(PNodo,string);
+
+int main(int argc, char *argv[]) {
+    string nombre;
+    cout<<"Ingrese una serie de nombres para ordenarlos alfabeticamente"<<endl;
+    do{
+        getline(cin,nombre);
+        if(nombre!= "0") insertarOrdenado(Lista,nombre);
+    }while(nombre != "0");
+    mostrarLista(Lista);
+    return 0;
+}
+
+void insertarAlPrincipio(PNodo& lista, string elemento){
+    PNodo ptr = new Nodo;
+    ptr->dato = elemento;
+    ptr->siguiente = lista;
+    lista = ptr;
+    return;
+}
+
+void insertarAlFinal(PNodo& lista, string elemento){
+    if(lista == NULL){
+        insertarAlPrincipio(lista,elemento);
+        return;
+    }
+    insertarAlFinal(lista->siguiente,elemento);
+}
+
+void insertarOrdenado(PNodo& Lista, string elemento){
     PNodo nuevo_elemento = new Nodo;
     nuevo_elemento->dato = elemento;
 
@@ -19,7 +60,7 @@ void insertarElemento(PNodo& Lista, string elemento){
         aux2 = aux1;
         aux1 = aux1->siguiente;
     }
-    //asd
+
     if(aux1 == Lista){
         Lista = nuevo_elemento;
     }else{
@@ -36,13 +77,89 @@ void mostrarLista(PNodo lista){
     }
 }
 
-int main(int argc, char *argv[]) {
-    string nombre;
-    cout<<"Ingrese una serie de nombres para ordenarlos alfabeticamente"<<endl;
-    do{
-        getline(cin,nombre);
-        if(nombre!= "0") insertarElemento(Lista,nombre);
-    }while(nombre != "0");
-    mostrarLista(Lista);
-    return 0;
+PNodo retornarElemento(PNodo lista, string elemento){
+    if(lista == NULL) return lista;
+    else if (elemento == lista->dato) return lista;
+    return retornarElemento(lista->siguiente,elemento);
+}
+
+void eliminarElemento(PNodo& lista, string elemento){
+    if(lista == NULL) cout<<"Elemento no encontrado"<<endl;
+    else if(lista->dato == elemento){
+        lista = lista->siguiente;
+        return;
+    }
+    else if (elemento == lista->siguiente->dato) {
+        PNodo aux = lista->siguiente;
+        lista->siguiente = lista->siguiente->siguiente;
+        delete aux;
+        return;
+    }
+    eliminarElemento(lista->siguiente,elemento);
+}
+
+bool buscarElemento(PNodo lista, string elemento){
+    if(lista == NULL) return false;
+    else if (elemento == lista->dato) return true;
+    return retornarElemento(lista->siguiente,elemento);
+}
+
+void eliminarTodosLosElementos(PNodo& lista){
+    if(lista == NULL) return;
+    PNodo siguiente = lista->siguiente;
+    delete lista;
+    lista = nullptr;
+    eliminarTodosLosElementos(siguiente);
+}
+
+void insertarPosicion(PNodo& lista, string elemento, int posicion){
+    if(posicion == 0){
+        insertarAlPrincipio(lista,elemento);
+        return;
+    }
+    if(posicion == 1){
+        PNodo ptr = new Nodo;
+        ptr->dato = elemento;
+        ptr->siguiente = lista->siguiente;
+        lista->siguiente = ptr;
+        return;
+    }
+    insertarPosicion(lista->siguiente,elemento,--posicion);
+}
+
+void eliminarPrimerPosicion(PNodo& lista){
+    PNodo aux = lista;
+    lista = lista->siguiente;
+    delete aux;
+}
+
+void eliminarPosicion(PNodo& lista, int posicion){
+    if(posicion == 0){
+        eliminarPrimerPosicion(lista);
+        return;
+    }
+    if(posicion == 1){
+        PNodo aux = lista->siguiente;
+        lista->siguiente = lista->siguiente->siguiente;
+        delete aux;
+        return;
+    }
+    eliminarPosicion(lista->siguiente,--posicion);
+}
+
+void eliminarUltimaPosicion(PNodo& lista){
+    if(lista->siguiente == nullptr){
+        lista = nullptr;
+        return;
+    }
+    eliminarUltimaPosicion(lista->siguiente);
+}
+
+void eliminarRepetidos(PNodo lista){
+    if(lista == nullptr) return;
+    string dato = lista->dato;
+    while(buscarElemento(lista->siguiente,dato)){
+        eliminarElemento(lista->siguiente,dato);
+    }
+    eliminarRepetidos(lista->siguiente);
 }
