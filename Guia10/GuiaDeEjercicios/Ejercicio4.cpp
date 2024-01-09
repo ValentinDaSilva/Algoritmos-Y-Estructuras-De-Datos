68e42b403894c57e6753226f62573226c4576289
ValentinDaSilva <valedasilvacatela.vdc@gmail.com>
2024-01-09 17:58:52 -0300

    Mensaje predeterminado

--- Guia10/GuiaDeEjercicios/Ejercicio4.cpp
+++ Guia10/GuiaDeEjercicios/Ejercicio4.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
+#include<iomanip>
 using namespace std;
+int cantAlumnos = 20;
 
 struct INTEGRACION{
     int ID;
@@ -18,71 +20,59 @@ typedef CALIFICACIONES* Alumno;
 void generarNotas(Alumno&);
 PIntegracion integrar(Alumno,Alumno);
 float promedio(float[],int);
+void mostrarLista(Alumno&);
+void mostrarIngreacion(PIntegracion);
+void insertarOrdenado(PIntegracion&,int,float);
 
 int main(){
     Alumno comisionA, comisionB;
     generarNotas(comisionA); generarNotas(comisionB);
+    // integrar(comisionA,comisionB);
     return 0;
 }
 float promedio(float v[], int TL){
     if(TL == 1) return v[0];
     return (v[0] + promedio(&v[1],TL-1) * (TL-1)) / (float) TL;
 }
+
 PIntegracion integrar(Alumno comisionA,Alumno comisionB){
-    float promedioAlumno = promedio(comisionA->calificacion,6);
     PIntegracion final = new INTEGRACION;
-
-    while(comisionA != nullptr){
-        PIntegracion nuevo_nodo = new INTEGRACION;
-        nuevo_nodo->ID = comisionA->ID;
-        nuevo_nodo->promedio = promedioAlumno;
-
-        PIntegracion aux1 = final;
-        PIntegracion aux2;
-
-        while(aux1 != nullptr and nuevo_nodo->promedio > final->promedio){
-            aux2 = aux1;
-            aux1 = aux1->siguiente;
-        }
-        if(aux1 == final) {
-            final = nuevo_nodo;
-        }else{
-            aux2->siguiente = nuevo_nodo;
-        }
-        nuevo_nodo->siguiente = aux1;
+    while(comisionA != NULL){
+        float promedioAlumno = promedio(comisionA->calificacion,6);
+        insertarOrdenado(final,comisionA->ID,promedioAlumno);
         comisionA = comisionA->siguiente;
     }
-    while(comisionB != nullptr){
-        PIntegracion nuevo_nodo = new INTEGRACION;
-        nuevo_nodo->ID = comisionA->ID;
-        nuevo_nodo->promedio = promedioAlumno;
-
-        PIntegracion aux1 = final;
-        PIntegracion aux2;
-
-        while(aux1 != nullptr and nuevo_nodo->promedio > final->promedio){
-            aux2 = aux1;
-            aux1 = aux1->siguiente;
-        }
-        if(aux1 == final) {
-            final = nuevo_nodo;
-        }else{
-            aux2->siguiente = nuevo_nodo;
-        }
-        nuevo_nodo->siguiente = aux1;
-        comisionB = comisionB->siguiente;
-    }
-
     return final;
 }
 
+void insertarOrdenado(PIntegracion& Lista, int ID, float promedio){
+    PIntegracion nuevo_elemento = new INTEGRACION;
+    nuevo_elemento->ID = ID;
+    nuevo_elemento->promedio = promedio;
+
+    PIntegracion aux1 = Lista;
+    PIntegracion aux2;
+
+    while(aux1 != NULL and aux1->promedio < promedio){
+        aux2 = aux1;
+        aux1 = aux1->siguiente;
+    }
+
+    if(aux1 == Lista){
+        Lista = nuevo_elemento;
+    }else{
+        aux2->siguiente = nuevo_elemento;
+    }
+    nuevo_elemento->siguiente = aux1;
+}
+
 void generarNotas(Alumno& alumno){
-    float flotantes[10] = {0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9};
-    for(int i = 0 ; i < 10 ; i++){
+    float flotantes[9] = {0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9};
+    for(int i = 0 ; i < 10 ; i++, cantAlumnos--){
         Alumno nuevo_nodo = new CALIFICACIONES;
-        nuevo_nodo->ID = i;
+        nuevo_nodo->ID = cantAlumnos;
         for(int j = 0; j < 6; j++){
-            nuevo_nodo->calificacion[j] = (float)(rand()%8+2) + flotantes[rand()%10];
+            nuevo_nodo->calificacion[j] = (float)(rand()%8+2) + flotantes[rand()%9];
         }
         nuevo_nodo->siguiente = alumno;
         alumno = nuevo_nodo;
