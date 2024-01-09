f5bc93703f606c5ebdc729d0aa3e0890d21bb08b
ValentinDaSilva <valedasilvacatela.vdc@gmail.com>
2024-01-08 21:06:01 -0300

    auto-commit

--- Guia10/ColasDinamicas.cpp
+++ Guia10/ColasDinamicas.cpp
@@ -12,12 +12,18 @@ void inicializarCola(Cola&,Cola&);
 bool estaVacia(Cola);
 void encolar(Cola&,Cola&,int);
 void desencolar(Cola&,Cola&);
-int tope(Cola);
 int tamanoCola(Cola);
 
 int main(){
     Cola miCola, frente;
-    
+    inicializarCola(miCola,frente);
+    encolar(miCola,frente,5);
+    encolar(miCola,frente,10);
+    encolar(miCola,frente,15);
+
+    cout<<frente->dato<<endl;
+    desencolar(miCola,frente);
+    cout<<frente->dato<<endl;
     return 0;
 }
 
@@ -30,16 +36,29 @@ bool estaVacia(Cola miCola){
     return miCola == nullptr;
 }
 
-void encolar(Cola& frente,Cola& fin, int elemento){
+void encolar(Cola& fin,Cola& frente, int elemento){
     Cola nuevo_nodo = new Nodo;
     nuevo_nodo->dato = elemento;
     nuevo_nodo->siguiente = nullptr;
 
     if(estaVacia(frente)){
-        fin = frente = nuevo_nodo;
+        frente = nuevo_nodo;
     }else{
         fin->siguiente = nuevo_nodo;
     }
     fin = nuevo_nodo;
+}
 
+void desencolar(Cola& fin,Cola& frente){
+    Cola aux = frente;
+    if(estaVacia(frente)) cout<<"No hay elementos para retirar"<<endl;
+    else{
+        if(frente == fin){
+            frente = fin = NULL;
+        }else{
+            frente = frente->siguiente;
+            delete aux;
+        }
+    }
+    return;
 }
