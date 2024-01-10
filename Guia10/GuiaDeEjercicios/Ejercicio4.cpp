d9b9e87b8c050a6e33f0fdb59b43fc212d84797b
ValentinDaSilva <valedasilvacatela.vdc@gmail.com>
2024-01-09 22:42:56 -0300

    Mensaje predeterminado

--- Guia10/GuiaDeEjercicios/Ejercicio4.cpp
+++ Guia10/GuiaDeEjercicios/Ejercicio4.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 #include <ctime>
+#include <iomanip>
 using namespace std;
-int cantAlumnos;
+int cantAlumnos, cantidad;
+
+struct Integrar
+{
+    int ID;
+    float promedio;
+    Integrar* siguiente;
+};
+typedef Integrar* PIntegrar;
 
 struct Calificacion {
     int ID;
@@ -13,22 +22,51 @@ typedef Calificacion* Alumno;
 void agregarElemento(Alumno&,int,float[]);
 void mostrarLista(Alumno);
 void cargarDatos(Alumno&);
+float promedio(float[],int);
+PIntegrar integrar(Alumno,Alumno);
+void mostrarListaIntegrada(PIntegrar);
+void agregarAIntegrar(PIntegrar&,int,float);
 
 int main(){
     Alumno comisionA = NULL, comisionB = NULL;
+    PIntegrar listaFinal = NULL;
     srand(time(NULL));
-    cout<<"Ingrese la cantidad de alumnos para generar datos aleatoriamente: "; cin>>cantAlumnos;
-    cargarDatos(comisionA);
-    mostrarLista(comisionA);
+    cout<<"Ingrese la cantidad de alumnos para generar datos aleatoriamente: "; cin>>cantAlumnos;  cantidad = cantAlumnos;cantAlumnos*=2;
+
+    cargarDatos(comisionA); cargarDatos(comisionB);
+    mostrarLista(comisionB); mostrarLista(comisionA);
+    listaFinal = integrar(comisionA,comisionB);
+    mostrarListaIntegrada(listaFinal);
     return 0;
 }
 
 void cargarDatos(Alumno& lista){
     float v[6];
-    for(int i = 0; i < cantAlumnos; i++){
+    for(int i = 0; i < cantidad; i++, cantAlumnos--){
         for(int j = 0 ; j < 6; j++) v[j] = (float)(rand()%10+1);
-        agregarElemento(lista,i,v);
+        agregarElemento(lista,cantAlumnos,v);
     }
+}
+
+void agregarAIntegrar(PIntegrar& Lista, int dato, float promedio){
+    PIntegrar nuevo_elemento = new Integrar;
+    nuevo_elemento->ID = dato;
+    nuevo_elemento->promedio = promedio;
+
+    PIntegrar aux1 = Lista;
+    PIntegrar aux2;
+
+    while(aux1 != NULL and nuevo_elemento->promedio < aux1->promedio){
+        aux2 = aux1;
+        aux1 = aux1->siguiente;
+    }
+
+    if(aux1 == Lista){
+        Lista = nuevo_elemento;
+    }else{
+        aux2->siguiente = nuevo_elemento;
+    }
+    nuevo_elemento->siguiente = aux1;
 }
 
 void agregarElemento(Alumno& Lista, int dato, float notas[]){
@@ -59,6 +97,33 @@ void mostrarLista(Alumno lista){
     cout<<lista->ID<<" [";
     for(int i = 0; i < 6; i++) cout<<lista->calificacion[i]<<" "; cout<<"]"<<endl;
     mostrarLista(lista->siguiente);
+}
+
+void mostrarListaIntegrada(PIntegrar lista){
+    if(lista == NULL){
+        cout<<endl;
+        return;
+    }
+    cout<<lista->ID<<" promedio: "<<fixed<<setprecision(2)<<lista->promedio<<endl;
+    mostrarListaIntegrada(lista->siguiente);
+}
+
+PIntegrar integrar(Alumno comisionA, Alumno comisionB){
+    PIntegrar listaFinal = NULL;
+    while(comisionA != NULL){
+        agregarAIntegrar(listaFinal,comisionA->ID,promedio(comisionA->calificacion,6));
+        comisionA = comisionA->siguiente;
+    }
+    while(comisionB != NULL){
+        agregarAIntegrar(listaFinal,comisionB->ID,promedio(comisionB->calificacion,6));
+        comisionB = comisionB->siguiente;
+    }
+    return listaFinal;
+}
+
+float promedio(float v[],int TL){
+    if(TL == 1) return v[0];
+    return (v[0] + (promedio(&v[1],TL-1)*(TL-1))) / TL; 
 }
 
 
