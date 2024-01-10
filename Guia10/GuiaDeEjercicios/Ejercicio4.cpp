2aba8197d74c9734e3461faf78440501bc645d7a
ValentinDaSilva <valedasilvacatela.vdc@gmail.com>
2024-01-09 21:53:48 -0300

    auto-commit

--- Guia10/GuiaDeEjercicios/Ejercicio4.cpp
+++ Guia10/GuiaDeEjercicios/Ejercicio4.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <ctime>
 using namespace std;
+int cantAlumnos;
 
 struct Calificacion {
     int ID;
@@ -11,14 +12,23 @@ typedef Calificacion* Alumno;
 
 void agregarElemento(Alumno&,int,float[]);
 void mostrarLista(Alumno);
+void cargarDatos(Alumno&);
 
 int main(){
-    float notas[] = {4.3,6.0,8.2,10.0,5.5,6.0};
     Alumno comisionA = NULL, comisionB = NULL;
     srand(time(NULL));
-    agregarElemento(comisionA,rand()%100,notas);
+    cout<<"Ingrese la cantidad de alumnos para generar datos aleatoriamente: "; cin>>cantAlumnos;
+    cargarDatos(comisionA);
     mostrarLista(comisionA);
     return 0;
+}
+
+void cargarDatos(Alumno& lista){
+    float v[6];
+    for(int i = 0; i < cantAlumnos; i++){
+        for(int j = 0 ; j < 6; j++) v[j] = (float)(rand()%10+1);
+        agregarElemento(lista,i,v);
+    }
 }
 
 void agregarElemento(Alumno& Lista, int dato, float notas[]){
