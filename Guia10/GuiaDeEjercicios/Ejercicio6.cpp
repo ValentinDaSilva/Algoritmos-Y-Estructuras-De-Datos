fb2668764b1c300a3642ed90dd4cce59142fa90e
ValentinDaSilva <valedasilvacatela.vdc@gmail.com>
2024-01-10 03:58:40 -0300

    auto-commit

--- Guia10/GuiaDeEjercicios/Ejercicio6.cpp
+++ Guia10/GuiaDeEjercicios/Ejercicio6.cpp
@@ -8,6 +8,12 @@ struct Nodo {
 };
 typedef Nodo* Lista;
 
+struct PILA {
+    int suma, fila;
+    PILA* siguiente;
+};
+typedef PILA* PPila;
+
 void agregarNodo(Lista&,int);
 void mostrarLista(Lista);
 void buscarAislados(int**,Lista&);
@@ -17,6 +23,7 @@ void mostrarMatriz(int**);
 int main(){
     int ** M;
     Lista listaAislados = NULL;
+    PPila sumaFilas = NULL;
     cout<<"Ingrese la cantidad de filas y columnas: ";cin>>dimensiones;
     M = new int* [dimensiones];
     for(int i = 0; i < dimensiones; i++) M[i] = new int[dimensiones];
@@ -53,6 +60,20 @@ void agregarNodo(Lista& lista, int dato){
     } 
 }
 
+void apilar(PPila& miPila, int suma, int fila){
+    PPila nuevo_nodo = new PILA;
+    nuevo_nodo->suma = suma;
+    nuevo_nodo->fila = fila;
+    nuevo_nodo->siguiente = miPila;
+    miPila = nuevo_nodo;
+}
+
+void desapilar(PPila& miPila){
+    PPila aux = miPila;
+    miPila = miPila->siguiente;
+    delete aux;
+}
+
 void mostrarLista(Lista lista){
     if(lista == NULL){
         cout<<" -> NULL"<<endl;
@@ -78,4 +99,28 @@ void mostrarMatriz(int** M){
         }
         cout<<"]"<<endl;
     }
+}
+
+void sumaFilas(int** M, PPila miPila){
+    int sumaPorFilas[dimensiones][2];
+    int suma = 0, fila;
+    for(int i = 0; i < dimensiones;i++){
+        for(int j = 0; j < dimensiones; j++){
+            suma += M[i][j];
+        }
+        sumaPorFilas[i][0] = i;
+        sumaPorFilas[i][1] = suma;
+        suma = 0;
+    }
+    Lista yaEsta = NULL;
+    for(int i = 0 ; i < dimensiones; i++){
+        
+    }
+
+}
+
+bool estaEnElLista(Lista lista, int valor){
+    if(lista == NULL) return false;
+    if(lista->elemento == valor) return true;
+    return estaEnElLista(lista->siguiente,valor);
 }
