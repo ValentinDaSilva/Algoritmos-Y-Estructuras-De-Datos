3d59b8a3ab2aa76e33655c1948ea1049dd02ec7c
ValentinDaSilva <valedasilvacatela.vdc@gmail.com>
2024-01-12 00:20:12 -0300

    auto-commit

--- Guia10/prueba papa.cpp
+++ Guia10/prueba papa.cpp
@@ -2,18 +2,12 @@
 #include <iostream>
 using namespace std;
 int main() {
-  ifstream archivo;
+  ofstream archivo;
   string nombre;
   cout<<"Ingrese el nombre del archivo: ";getline(cin,nombre);
-  archivo.open(nombre.c_str());
-if(archivo.fail()) cout<<"fallo en abrir el archivo"<<endl;
-  // Leer el archivo
-  char caracter;
-  while (archivo.get(caracter)) {
-    cout << caracter;
-  }
-
+  archivo.open(nombre.c_str(),ios::app);
+  archivo<<"Hola Como Estas"<<endl;
   archivo.close();
-
+  archivo.clear();
   return 0;
 }
