c713793a1ffd2d58e283173f0b18931182eba4c8
ValentinDaSilva <valedasilvacatela.vdc@gmail.com>
2024-01-12 00:52:40 -0300

    auto-commit

--- Guia10/prueba papa.cpp
+++ Guia10/prueba papa.cpp
@@ -2,12 +2,18 @@
 #include <iostream>
 using namespace std;
 int main() {
-  ofstream archivo;
+  ifstream archivo;
   string nombre;
   cout<<"Ingrese el nombre del archivo: ";getline(cin,nombre);
+  nombre = "/home/valedasilvacatelavdc/Algoritmos-Y-Estructuras-De-Datos/Guia10/" + nombre;
   archivo.open(nombre.c_str(),ios::app);
-  archivo<<"Hola Como Estas"<<endl;
+  if(archivo.fail()) cout<<"error al abrir el archivo."<<endl;
+  char caracter;
+  do{
+    archivo >> caracter;
+    cout<<caracter;
+  }while(!archivo.eof());
+  cout<<endl;
   archivo.close();
-  archivo.clear();
   return 0;
 }
