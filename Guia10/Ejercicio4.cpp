b08d735bd0e7d4110f600b59b443388a154e3855
ValentinDaSilva <valedasilvacatela.vdc@gmail.com>
2024-01-13 17:22:40 -0300

    auto-commit

--- Guia10/Ejercicio4.cpp
+++ Guia10/Ejercicio4.cpp
@@ -4,16 +4,38 @@ using namespace std;
 
 void alternativa1(ofstream&,int);
 void alternativa2(ofstream&,int);
+void leerAlternativa1(ifstream&);
 
 int main(){
-    int valor;
+    int valor = 5;
     ofstream archivo("archivoBinario.bin",ios::trunc|ios::binary);
-    
+    alternativa1(archivo,valor);
+    archivo.close();
+    ifstream archivo2("archivoBinario.bin",ios::binary);
+    if(archivo2.fail()) cout<<"No se pudo abrir el archivo"<<endl;
+    leerAlternativa1(archivo2);
+    archivo2.close();
     return 0;
 }
 
-void leerAlternativa1(ofstream& archivo){
-    
+void leerAlternativa1(std::ifstream& archivo) {
+    archivo.seekg(0, std::ios::end);
+    int tamanio = archivo.tellg() / sizeof(int);
+    archivo.seekg(0, ios::beg);
+    int numero, i = 0;
+    while(i < tamanio){
+        archivo.read((char*)&numero,sizeof(int));
+        cout<<numero<<" ";
+        i++;
+    }
+    /* Alternativa
+    int numero;
+    while(archivo.good()){
+        archivo.read((char*)&numero,sizeof(int));
+        if(archivo.tellg() != EOF) cout<<numero<<" ";
+    }   
+     */
+    cout<<endl;
 }
 
 void alternativa1(ofstream& archivo,int valor){
@@ -27,6 +49,6 @@ void alternativa1(ofstream& archivo,int valor){
 
 void alternativa2(ofstream& archivo,int valor){
     for(int i = 0; i < valor ; i++){
-        archivo.write((char*)i,sizeof(int)); 
+        archivo.write((char*)&i,sizeof(int)); 
     }
 }
