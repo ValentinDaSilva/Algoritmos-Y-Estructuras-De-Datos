fe9e305e4c35c98e105231de310f526e135d0385
ValentinDaSilva <valedasilvacatela.vdc@gmail.com>
2024-01-13 16:57:58 -0300

    auto-commit

--- Guia10/Ejercicio4.cpp
+++ Guia10/Ejercicio4.cpp
@@ -1,7 +1,32 @@
 #include<iostream>
+#include<fstream>
 using namespace std;
 
+void alternativa1(ofstream&,int);
+void alternativa2(ofstream&,int);
+
 int main(){
+    int valor;
+    ofstream archivo("archivoBinario.bin",ios::trunc|ios::binary);
     
     return 0;
+}
+
+void leerAlternativa1(ofstream& archivo){
+    
+}
+
+void alternativa1(ofstream& archivo,int valor){
+    cout<<"Ingrese hasta que numero desea contar: ";cin>>valor;
+    int* vector = new int[valor];
+    for(int i = 0; i < valor; i++){
+        vector[i] = i;
+    }
+    archivo.write((char *)vector,sizeof(vector));
+}
+
+void alternativa2(ofstream& archivo,int valor){
+    for(int i = 0; i < valor ; i++){
+        archivo.write((char*)i,sizeof(int)); 
+    }
 }
