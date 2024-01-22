1e7e94e43abc3f7e86628d6d0f5994c745e89a7d
ValentinDaSilva <valedasilvacatela.vdc@gmail.com>
2024-01-22 00:40:47 -0300

    auto-commit

--- Parciales-Finales/Finales /6_4_2022/6_4_2022.cpp
+++ Parciales-Finales/Finales /6_4_2022/6_4_2022.cpp
@@ -1,7 +1,32 @@
 #include<iostream>
 using namespace std;
 
+struct LINEA{
+    int numero = 0;
+    string texto = "";
+};
+typedef LINEA* Linea;
+
+struct CODIGOFUENTE {
+    Linea lineas = new LINEA;
+};
+
+struct ENTREGA {
+    int IDEstrudiante = 0;
+    CODIGOFUENTE CodigoFuente;
+};
+
+struct COMISION{
+    char nombre = ' ';
+    ENTREGA Entregas[250];
+    int cantEstudiantes = 0;
+};
+
 int main(){
-    
+    COMISION A,B,C;
+    A.cantEstudiantes = 125;
+    B.cantEstudiantes = 145;
+    C.cantEstudiantes = 198;
+
     return 0;
 }
